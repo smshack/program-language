2f61ff5499e09c2eaf0dac54a79b2483e829b768
smshack <5432tat@naver.com>
2022-09-05 15:29:00 +0900

    1. 콘솔에 출력,script asnync 와 defer의 차이점 및 앞으로 자바스크립트 공부 방향

---
+++ node-javascript-basic/1helloworld.js
@@ -0,0 +1,8 @@
+'use strict';
+console.log('------------------------------------');
+console.log("hello world");
+console.log("hihi");
+const person = { name:"jarry", sex : "male"};
+
+console.log('person',person)
+console.log('------------------------------------');
