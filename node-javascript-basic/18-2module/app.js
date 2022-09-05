45dccd54eb377b99e27fade93ab55bfdc39f6a69
smshack <5432tat@naver.com>
2022-09-05 16:37:45 +0900

    ## 18. 모듈 ``` 프로젝트를 만들 때 연관 있는 것들은 한 디렉터리 내에 모듈로 만들어서 묶어 놓는 것이 맞음
    ---------------------------------------------------
    exports
    require
    ----------------------------------------------------
    npm init -y
    type:'module'
    
    export function increate(){
    
    }
    import {increase} from ./counter
    ```

---
+++ node-javascript-basic/18-2module/app.js
@@ -0,0 +1,12 @@
+import { increase, getCount } from './counter.js'
+
+increase();
+increase();
+increase();
+console.log(getCount())
+
+// import * as counter from './counter.js'
+// counter.increase();
+// counter.increase();
+// counter.increase();
+// console.log(counter.getCount())
