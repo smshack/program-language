1deaa8a582f73911b68b641464919958b3fd0551
smshack <5432tat@naver.com>
2022-09-05 17:45:23 +0900

    #30. 그래픽

---
+++ java-basic/src/lecture30/ex1/MakeBtn.java
@@ -0,0 +1,13 @@
+package lecture30.ex1;
+
+import java.awt.Button;
+import java.awt.Frame;
+
+public class MakeBtn extends Frame{
+	
+	public MakeBtn() {
+		Button btn = new Button("Button");
+		add(btn);
+	}
+	
+}
---
+++ java-basic/src/lecture30/ex3/MainClass.java
@@ -0,0 +1,14 @@
+package lecture30.ex3;
+
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		MakeComp makeComp = new MakeComp();
+		makeComp.pack();
+		makeComp.setVisible(true);
+		
+	}
+	
+}
