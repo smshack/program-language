8a65b003475058547f7d8c6cabbe1e94223a3668
smshack <5432tat@naver.com>
2022-09-06 12:40:34 +0900

    # 불과 비교 논리 연산자 알아보기

---
+++ python-basic/5boolean/boolean.py
@@ -0,0 +1,36 @@
+print('1 ---------------------------')
+print(True)
+print(False)
+print(3>1)
+print('2 ---------------------------')
+print(10 == 10)
+print(10 != 5)
+print('3 ---------------------------')
+print('Python' == 'Python')
+print('Python' != 'python')
+print('4 ---------------------------')
+print( 10 > 20  )  # 10이 20보다 큰지 비교
+print( 10 < 20  )  # 10이 20보다 작은지 비교
+print( 10 >= 10 )   # 10이 10보다 크거나 같은지 비교
+print( 10 <= 10 )   # 10이 10보다 작거나 같은지 비교
+
+print('5 ---------------------------')
+print( 1 == 1.0)
+print( 1 is 1.0)
+print( 1 is not 1.0)
+
+print('6 ---------------------------')
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print(not True)
+print(not False)
+
+print(not True and False or not False)
